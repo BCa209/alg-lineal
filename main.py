328fc174e35ad75a2ff05a0b88a9ed623536841f
BCa209 <156900278+BCa209@users.noreply.github.com>
2025-02-27 04:38:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+import streamlit as st
+from app import *
+
+st.set_page_config(
+    page_title="Combinaciones Lineales", 
+    page_icon="📚"
+)
+
+main()  # Llama a la función principal del otro archivo
